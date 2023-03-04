aaeec761df678189bad901109d6b4c4ac7e0d252
Jeelan9686 <shaikjeelan533526@gmail.com>
2023-03-04 12:53:06 +0530

    commit

--- src/com/khadri/hibernate/client/Client3.java
+++ src/com/khadri/hibernate/client/Client3.java
@@ -3,21 +3,22 @@ package com.khadri.hibernate.client;
 import java.util.Scanner;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import com.khadri.hibernate.entity.College;
+import com.khadri.hibernate.session.BasicOperationSession;
 
 public class Client3 {
 	private static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		Configuration cfg = new Configuration();
-		cfg.configure();
-		SessionFactory sf = cfg.buildSessionFactory();
-		Session sess = sf.openSession();
-		System.out.println("please enter college Id...");
+ 
+		Session sess =  BasicOperationSession.getSession();
+		
+		System.out.println("please enter college Id : ");
 		int id = sc.nextInt();
+		
 		College cid = sess.get(College.class, id);
+		
+		
 		System.out.println(cid.getClgId());
 		System.out.println(cid.getClgName());
 		
