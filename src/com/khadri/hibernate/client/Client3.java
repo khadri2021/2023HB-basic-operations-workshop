6bf5799c5de0099932c5a8731c42b7ddd6dbf8e8
Jeelan9686 <shaikjeelan533526@gmail.com>
2023-03-05 10:25:17 +0530

    committed client3

--- src/com/khadri/hibernate/client/Client3.java
+++ src/com/khadri/hibernate/client/Client3.java
@@ -1,5 +1,6 @@
 package com.khadri.hibernate.client;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 import org.hibernate.Session;
@@ -9,19 +10,26 @@ import com.khadri.hibernate.session.BasicOperationSession;
 
 public class Client3 {
 	private static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
- 
-		Session sess =  BasicOperationSession.getSession();
-		
+
+		Session sess = BasicOperationSession.getSession();
+
 		System.out.println("please enter college Id : ");
 		int id = sc.nextInt();
-		
+
 		College cid = sess.get(College.class, id);
-		
-		
-		System.out.println(cid.getClgId());
-		System.out.println(cid.getClgName());
-		
+
+		Optional<College> ofNullable = Optional.ofNullable(cid);
+		if (ofNullable.isPresent()) {
+			College cid2 = ofNullable.get();
+
+			System.out.println(cid2.getClgId());
+			System.out.println(cid2.getClgName());
+		} else {
+			System.out.println("NO RECORDS FOUND !!!!");
+		}
+
 	}
 
 }
