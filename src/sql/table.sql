7de58ada9adcaccd6894e42f21a30222c6d39c0f
vikram-8790 <vickypandu9@gmail.com>
2023-03-05 11:03:29 +0530

    student sql table added

--- src/sql/table.sql
+++ src/sql/table.sql
@@ -9,3 +9,8 @@ create table employee(empId int,empName varchar(12),salary double(8,2));
 select * from employee;
 -- Client1 bug fix changes KHADRI-1-BUG-1
 alter table employee rename column salary to empSalary;
+
+-- client4 changes KHADRI-4
+create table student(stdId int,stdName varchar(15),fatherName varchar(15),town varchar(15),district varchar(15),fee double(7,2),percentage int,collegeName varchar(15));
+
+select * from student;
